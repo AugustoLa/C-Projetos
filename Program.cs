31d727362a5e854f3b6a3b555d8978355728e196
Augusto Laursen <augusto_florindo@hotmail.com>
2022-07-06 11:16:31 -0300

    Criando a classe posição xadrez

--- Program.cs
+++ Program.cs
@@ -4,10 +4,10 @@ using Xadrez_Console;
 
 Tabuleiro tab = new Tabuleiro(8, 8);
 
-tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0,0));
-tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2,4));
 
-Tela.imprimirTabuleiro(tab);
+PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-Console.WriteLine(tab);
+Console.WriteLine(pos);
+
+
+Console.WriteLine(pos.toPosicao());
