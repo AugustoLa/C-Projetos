6af19a05b0760c6eada842c3287a9685ee94de45
AugustoLa <augusto_florindo@hotmail.com>
2022-07-08 01:05:08 -0300

    Correção de bug
    Resolução de bug caso escolhesse as colunas com letra maiúscula

--- Tela.cs
+++ Tela.cs
@@ -133,7 +133,7 @@ namespace xadrez_Console
 
         public static PosicaoXadrez lerPosicaoXadrez()
         {
-            string s = Console.ReadLine();
+            string s = Console.ReadLine().ToLower();
 
             char coluna = s[0];
             int linha = int.Parse(s[1] + "");
